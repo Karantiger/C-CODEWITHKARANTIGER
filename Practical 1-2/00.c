b6d78eeb679d69a34f51260f039ec3fdac3818f7
Karantiger <134210243+Karantiger@users.noreply.github.com>
2024-05-08 10:31:02 +0530

    Delete Practical 1-2 directory

--- Practical 1-2/00.c
+++
@@ -1,8 +0,0 @@
-//Program for write "Hello World" in c
-
-#include<stdio.h>
-int main()
-{
-    printf("Hello World");
-    return 0;
-}
--- Practical 1-2/05.c
+++
@@ -1,9 +0,0 @@
-// Program operation of character data type (Static)
-
-#include<stdio.h>
-int main()
-{
-    char ch='a';
-    printf("Given character=%c\n", ch);
-    return 0;
-}
