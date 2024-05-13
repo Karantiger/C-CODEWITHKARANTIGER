2ecf330560f5331e51878e8231d6bcd1ac75ab47
Karantiger <134210243+Karantiger@users.noreply.github.com>
2024-05-13 09:09:58 +0530

    Update area&circumferencneofcircle.c

--- C Practical/area&circumferencneofcircle.c
+++ C Practical/area&circumferencneofcircle.c
@@ -13,3 +13,12 @@ int main()
     printf("The circumference of the circle is: %f",circumference);
     return 0;
 }
+
+/*Output*/
+/*
+Enter the radius of the circle: 5
+The area of the circle is: 78.500000
+The circumference of the circle is: 31.400000
+
+=== Code Execution Successful ===
+*/
